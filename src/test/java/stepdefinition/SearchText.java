3b5899463666692686f2c472d9a612a02c6c9245
anilmishra9305 <k23.anil@gmail.com>
2020-02-13 11:36:00 +0530

    co
    co

---
+++ src/test/java/stepdefinition/SearchText.java
@@ -0,0 +1,18 @@
+package stepdefinition;
+
+import cucumber.api.java.en.When;
+import main.CucumberRunner;
+import pages.SearchPage;
+
+public class SearchText extends CucumberRunner {
+
+	SearchPage page = new SearchPage();
+
+	@When("^I type \"(.*?)\"$")
+	public void searchText(String text) throws Throwable {
+		explicitWait(page.searchBox);
+		page.searchBox.sendKeys(text);
+
+	}
+
+}
